2f260ae446ec0e16c4514793403604447dfa98f4
Robert DeLuca <robertdeluca19@gmail.com>
2019-02-26 11:04:33 -0600

    Update launch options for CI
    Of course I forgot that.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer');
 const {percySnapshot} = require('@percy/puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    // for CI
+    args: ['–no-sandbox', '–disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   await page.goto('https://percy.io');
